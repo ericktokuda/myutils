b3f528bdf30cb92af5d8f5e865430b48fa6e460d
Eric K. Tokuda <eric.tokuda@maths.ox.ac.uk>
2022-11-17 12:11:20 +0000

    63

--- myutils/__init__.py
+++ myutils/__init__.py
@@ -1,5 +1,7 @@
 import sys, os
 from datetime import datetime
+from multiprocessing import Pool
+from subprocess import Popen, DEVNULL, PIPE
 
 def info(*args):
     pref = datetime.now().strftime('[%H:%M:%S]')
@@ -16,3 +18,23 @@ def create_readme(argv, outdir):
 def append_to_file(file, text):
     info('{}'.format(text))
     os.system('echo "{}" >> "{}"'.format(text, file))
+
+def parallelize(func, nprocs, argsconcat):
+    if nprocs == 1:
+        return [ func(*args) for args in argsconcat ]
+    else:
+        info('Running in parallel ({} procs)'.format(nprocs))
+        with Pool(processes=nprocs) as pool:
+            return pool.starmap(func, argsconcat)
+
+def run_bash_cmd(cmd, verb=True, sync=True):
+    if verb: info(cmd)
+    if sync:
+        p = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        output, err = p.communicate()
+        if verb:
+            info('OUTPUT:{}, ERR:{}'.format(
+                output.decode('utf-8'), err.decode('utf-8')))
+    else:
+        Popen(cmd, shell=True, stdout=DEVNULL, stderr=DEVNULL)
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="erikunicamp-myutils",
-    version="0.0.61",
+    version="0.0.63",
     author="erikunicamp",
     author_email="erik.unicamp@gmail.com",
     description="Utilities package",
